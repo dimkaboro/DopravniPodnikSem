f89f52cfaba3b6e70362b30ba4575f7a11ace3db
Dmytro <dmitrysag1606@gmail.com>
2024-10-25 14:51:17 +0200

    added half Models

---
+++ Models/Faktura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DopravniPodnikSem.Models
+{
+    public class Faktura : DopravniPlatba
+    {
+        private int _cislouctu;
+        private DateTime _datumsplatnosti;
+
+        public int CisloUctu
+        {
+            get => _cislouctu;
+            set => SetField(ref _cislouctu, value);
+        }
+
+        public DateTime DatumSplatnosti
+        {
+            get => _datumsplatnosti;
+            set => SetField(ref _datumsplatnosti, value);
+        }
+    }
+}
---
+++ Models/Zamestnanec.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DopravniPodnikSem.Models
+{
+    public class Zamestnanec : BaseModel
+    {
+        private int _zamestnanecId;
+        private string _jmeno;
+        private string _prijmeni;
+        private string _pozice;
+        private decimal _plat;
+        private DateTime _datumNastupu;
+        private int _zamestnanecZamestnanecId;
+
+        public int ZamestnanecId
+        {
+            get => _zamestnanecId;
+            set => SetField(ref _zamestnanecId, value);
+        }
+
+        public string Jmeno
+        {
+            get => _jmeno;
+            set => SetField(ref _jmeno, value);
+        }
+
+        public string Prijmeni
+        {
+            get => _prijmeni;
+            set => SetField(ref _prijmeni, value);
+        }
+
+        public string Pozice
+        {
+            get => _pozice;
+            set => SetField(ref _pozice, value);
+        }
+
+        public decimal Plat
+        {
+            get => _plat;
+            set => SetField(ref _plat, value);
+        }
+
+        public DateTime DatumNastupu
+        {
+            get => _datumNastupu;
+            set => SetField(ref _datumNastupu, value);
+        }
+
+        public int ZamestnanecZamestnanecId
+        {
+            get => _zamestnanecZamestnanecId;
+            set => SetField(ref _zamestnanecZamestnanecId, value);
+        }
+    }
+}
